70d2fa9e598276243788161b49f0ae8a50c38c99
uphanda <nkosinathimagubane1@gmail.com>
2021-02-12 11:49:48 +0200

    second commit

--- Task0_8.js
+++ Task0_8.js
@@ -1,12 +1,10 @@
-function numbertToTime(number)
- { 
-  const hours = Math.floor(number / 60);  
-  const minutes = number % 60;
-  return hours + ' hours' + ',' + minutes + ' minutes' ;         
+function numbertToTime(number) {
+    const hours = Math.floor(number / 60);
+    const minutes = number % 60;
+    return hours + ' hour' + ',' + minutes + ' minute';
 }
 
 let time = numbertToTime(71);
 console.log(time);
 time = numbertToTime(133);
 console.log(time);
-
