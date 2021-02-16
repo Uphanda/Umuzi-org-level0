3c216db3b7f492611c1f98c28d42a812fbb46e0f
Nkosinathi Magubane <74439606+Uphanda@users.noreply.github.com>
2021-02-16 17:56:16 +0200

    Update Task0_8.js

--- Task0_8.js
+++ Task0_8.js
@@ -1,9 +1,14 @@
 function numbertToTime(number) {
-    const hours = Math.floor(number / 60);
+  const hours = Math.floor(number / 60);
     const minutes = number % 60;
-    return hours + ' hour' + ',' + minutes + ' minute';
-}
+    if (hours >= 2 || hours == 0) {
+        return hours + ' hours' + ',' + minutes + ' minutes';
+    } else if (hours == 1) {
+        return hours + ' hour' + ',' + minutes + ' minutes';
+    }
 
+
+}
 let time = numbertToTime(71);
 console.log(time);
 time = numbertToTime(133);
