2dbd1308465131bd6b8ca89114b8034fbf5ebb59
Nkosinathi Magubane <74439606+Uphanda@users.noreply.github.com>
2021-02-19 19:36:50 +0200

    Update Task0_10.js

--- Task0_10.js
+++ Task0_10.js
@@ -1,13 +1,20 @@
 function outputCommonChars(str1, str2) {
-    let commonChars = '';
-    for (let i = 0; i < str1.length; i++) {
-        for (let j = 0; j < str2.length; j++) {
-            if (str1.charAt(i) == str2.charAt(j)) {
-                commonChars = commonChars + str1.charAt(i);
-            }
-        }
-    }
-    return commonChars;
-}
-let characters = outputCommonChars('nkosi', 'nathi');
-console.log(characters.split('').toString());
+
+     let commonChars = '';
+
+     for (let i = 0; i < str1.length; i++) {
+         for (let j = 0; j < str2.length; j++) {
+             if (str1.charAt(i) == str2.charAt(j)) {
+                 if (commonChars.indexOf(str1.charAt(i)) == -1) {
+                     commonChars = commonChars + str1.charAt(i);
+                 }
+
+             }
+         }
+     }
+     return commonChars;
+
+
+ }
+ let characters = outputCommonChars('nkosiiiiii', 'nathni');
+ console.log(characters.split('').toString());
